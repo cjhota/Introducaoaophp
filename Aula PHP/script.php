d1cfc7b065b7d34ba22b675ff12d19cc1c5e7a14
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-10-18 11:48:54 -0300

    regras de validação do formulario

--- Aula PHP/script.php
+++ Aula PHP/script.php
@@ -8,6 +8,31 @@
 
     $nome = $_POST['name'];
     $idade = $_POST['idade'];
+
+    if(empty($name)) 
+{
+    echo'O nome não pode ser vazio';
+    return;
+}
+
+if(strlen($name)<3)
+{
+    echo ' O nome deve conter mais de 3 caracters';
+    return;
+}
+
+if(strlen($name) > 40)
+{
+    echo ' O nome é muito extenso';
+    return;
+}
+
+if(!is_numeric($idade))
+{
+    echo "Informe um número valido";
+    return;
+}
+
     // var_dump($nome);
     // var_dump($idade);
 
