254e919d9cf6a276b89dbad0436be94767653890
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-10-18 12:30:06 -0300

    mundaça nas regra de validação

--- Aula PHP/Indexphaula2.php
+++ Aula PHP/Indexphaula2.php
@@ -1,19 +1,33 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <p>formulario para criação de competidores </p>
 
     <form action="script.php" method="post">
+        <?php 
 
-    <p>Your nome <input type="text" name="name"></p>
-    <p>Your idade <input type="text" name="idade"></p>
-    <p><input type="submit" value="Enviar dados do competidor"></p>
+            $mensagemDeErro = isset($_SESSION['mensagem de erro']) ? $_SESSION['mensagem de erro'] : '';
+            if(!empty($mensagemDeErro))
+            {
+                echo $mensagemDeErro;
+            }
+        ?>
+        <p>Your nome <input type="text" name="name"></p>
+        <p>Your idade <input type="text" name="idade"></p>
+        <p><input type="submit" value="Enviar dados do competidor"></p>
     </form>
 </body>
+
 </html>
